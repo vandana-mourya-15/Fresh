c3baea5caf87290237e280992adbcbee841f1bc0
vandana-mourya-15 <vandanamourya2004@gmail.com>
2024-11-21 16:46:33 +0530

    update port to 5000

--- app.py
+++ app.py
@@ -109,6 +109,6 @@ if __name__ == '__main__':
         os.makedirs('uploads')
 
     # Use the PORT environment variable for dynamic port binding (for platforms like Render)
-    port = int(os.environ.get('PORT', 10000))  # Default to 5000 for local development
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local development
     app.run(debug=True, host='0.0.0.0', port=port)  # Ensure it listens on all interfaces
 
